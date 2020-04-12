0e8415e2865fde94d746aa5a9849a56829cf5a44
tosigame <tosigame@gmail.com>
2020-04-12 13:45:02 +0300

    purple

--- Assets/Scripts/DrawTest.cs
+++ Assets/Scripts/DrawTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DrawTest : MonoBehaviour
 {
      public Texture2D texture;
+    public Texture2D textureMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,13 +13,25 @@ public class DrawTest : MonoBehaviour
         // texture = GetComponent<SpriteRenderer>().sprite.texture;
         Sprite sprite = Sprite.Create(texture,new Rect(0,0,texture.width,texture.height),Vector2.zero);
         GetComponent<SpriteRenderer>().sprite = sprite;
-        for (int y = 100; y < 300; y++)
+        Color pixel;
+        for (int y = 0; y < texture.height; y++)
         {
 
 
-            for (int x = 100; x < 300; x++)
+            for (int x = 0; x < texture.width; x++)
             {
-                texture.SetPixel(x, y, new Color(0.5f, 0.84f, .9f, 0.88f));
+                pixel = textureMask.GetPixel(x, y);
+                    //Debug.Log(pixel);
+                if (pixel.r > 0.665 && pixel.r < 0.67f && pixel.g > 0.85 && pixel.g < 0.86f && pixel.b == 1.000f ) 
+                {
+                   // Debug.Log("well ya");
+                    texture.SetPixel(x, y, new Color(0.9f, 0f, 1f, 1f));
+
+                }
+                
+                // RGBA(0.667, 0.855, 1.000, 1.000) water color (google maps)
+                
+
             }
         }
         texture.Apply();
